2b23cec01684dec2ff35e7ef873febeb31122747
MugheesQasim <mughees.qasim.mq@gmail.com>
2025-04-06 17:58:17 +0300

    Add command line arguments

--- scripts/benchmark.c
+++ scripts/benchmark.c
@@ -1,7 +1,37 @@
+#include <stdio.h>
 #include <stdlib.h>
 
-int main() {
-    for (int i = 0; i < 100; i++) {
-        system("../target/release/rust-ml-benchmark \"../assets/models/mobilenetv2-10.onnx\" \"../assets/imgs/unseen_dog.jpg\"");
+int main(int argc, char *argv[])
+{
+    if (argc != 4)
+    {
+        fprintf(stderr, "Usage: %s <num_iterations> <model_path> <image_path>\n", argv[0]);
+        return 1;
+    }
+
+    const num_iterations = atoi(argv[1]);
+
+    if (num_iterations <= 0)
+    {
+        fprintf(stderr, "Error: Number of iterations must be a positive integer\n");
+        return 1;
+    }
+
+    const char *model_path = argv[2];
+    const char *image_path = argv[3];
+
+    if (strlen(model_path) == 0 || strlen(image_path) == 0)
+    {
+        fprintf(stderr, "Error: Model path or image path could not be empty \n");
+        return 1;
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        char command[1024];
+        snprintf(command, sizeof(command),
+                 "../target/release/rust-ml-benchmark \"%s\" \"%s\"",
+                 model_path, image_path);
+        system(command);
     }
 }
